0f4807950f66fad680995dba435ae286dfbc9f80
bparker <ben@zorbitsmath.com>
2021-12-01 11:15:27 -0330

    day 1 solutions

--- Day1.R
+++ Day1.R
@@ -1,1 +1,19 @@
 library(tidyverse)
+library(zoo)
+
+getInput = function(day) {
+  read_csv(paste("inputs/day", day, ".txt", sep = ""), col_names = c("input"))
+}
+
+day1Data = getInput(1)
+
+day1Data = day1Data %>%
+  mutate(deltaPlus = input > lag(input, n = 1, default = NA))
+
+day1Ans1 = sum(day1Data$deltaPlus, na.rm = TRUE)
+
+day1Data = day1Data %>%
+  mutate(slidingSum = rollsum(input, k=3, na.pad = TRUE, align = "right")) %>%
+  mutate(slidingDeltaPlus = slidingSum > lag(slidingSum, n = 1, default = NA))
+
+day1Ans2 = sum(day1Data$slidingDeltaPlus, na.rm = TRUE)
